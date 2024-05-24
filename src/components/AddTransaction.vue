07242c776b85db752383c391d5cb29d784bb7a43
Kyaw1511 <kyawminthein151193@gmail.com>
2024-05-24 19:04:39 +0630

    complete 5 components build

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,28 @@
+<template>
+    <h3>
+        Add new transaction
+    </h3>
+    <form id="form" action="">
+        <div class="form-control">
+            <input 
+                type="text"
+                id="text"
+                placeholder="Enter text..."
+            >
+        </div>
+        <div class="form-control">
+            <label for="amount">
+                >Amount <br/>
+                (negative - expense, positive - income)
+            </label>
+            <input 
+                type="number"
+                id="amount"
+                placeholder="Enter amount..."
+            >
+        </div>
+        <button class="btn">
+            Add transaction 
+        </button>
+    </form>
+</template>
---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="inc-exp-container">
+        <!-- income catagory -->
+        <div>
+            <h4>Income</h4>
+            <p id="money-plus"
+                class="money plus"
+            >
+                $+0.00
+            </p>
+        </div>
+        <!-- expense category -->
+        <div>
+            <h4>Expense</h4>
+            <p 
+                id="money-minus"
+                class="money minus"
+            >
+                $-0.00
+            </p>
+        </div>
+    </div>
+</template>
