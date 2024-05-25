86845cbeda0b2ecd1f650e086cce1356dec9f6a4
Kyaw1511 <kyawminthein151193@gmail.com>
2024-05-25 20:15:17 +0630

    balance component and computed

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -4,19 +4,71 @@
         id="list"
         class="list"
     >
-        <li class="minus">
-            Cash 
-            <span>-$400</span>
-            <button class="delete-btn">
-                x
-            </button>
-        </li>
-        <li class="plus">
-            Paycheck
-            <span>$800</span>
+        <li 
+            v-for="transaction in transactions" 
+            :key="transaction.id"
+            :class="transaction.amount < 0 ? 'minus' : 'plus'"
+        >
+            {{ transaction.text }}
+            <span>$ {{ transaction.amount }} </span>
             <button class="delete-btn">
                 x
             </button>
         </li>
     </ul>
 </template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps ({
+        transactions: {
+            type: Array,
+            required: true,
+        },
+    });
+</script>
+
+<!-- using Option API -->
+<!-- <script>
+    export default {
+        data() {
+            return {
+                transactions: [
+                    { id: 1, text: 'Flower', amount: -19.99 }, 
+                    { id: 2, text: 'Salary', amount: 299.97 },
+                    { id: 3, text: 'Book', amount: -10 },
+                    { id: 4, text: 'Camera', amount: 150 },
+                ],
+            };
+        },
+    };
+</script> -->
+
+<!-- Composition script setup -->
+<!-- <script setup>
+    const transactions = [
+        { id: 1, text: 'Flower', amount: -19.99 }, 
+        { id: 2, text: 'Salary', amount: 299.97 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 },
+    ];
+</script> -->
+
+<!-- Composition API v3.2 -->
+<!-- <script>
+    export default {
+        setup() {
+            const transactions = [
+                { id: 1, text: 'Flower', amount: -19.99 },
+                { id: 2, text: 'Salary', amount: 299.97 },
+                { id: 3, text: 'Book', amount: -10 },
+                { id: 4, text: 'Camera', amount: 150 },
+            ]
+        }
+    };
+
+    return {
+        transactions,
+    }
+</script> -->
